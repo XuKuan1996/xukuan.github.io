11b8b8dd3a2bb45d67c2304f7e163a0dd7d82fcc
24482 <kuan.xu@hand-china.com>
2019-07-29 10:29:39 +0800

    update project

---
+++ precache-manifest.253282a5b74564da40520c947c946638.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6ffc41cebc015a6ec0d95012e9880e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60fbb7b7010fb49c15a6",
+    "url": "/static/css/main.3dc94cb9.chunk.css"
+  },
+  {
+    "revision": "4e3f8ac0f0dfac5d1de0",
+    "url": "/static/js/2.096281fd.chunk.js"
+  },
+  {
+    "revision": "60fbb7b7010fb49c15a6",
+    "url": "/static/js/main.d331fabd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "980d8249c9d32917da490180f5ac288c",
+    "url": "/static/media/bg.980d8249.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
